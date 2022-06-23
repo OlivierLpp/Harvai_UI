60bd013e403438738085b462b0dde21a0ffd8121
Olivier LePestipon <o.lepestipon@gmail.com>
2022-06-23 15:29:19 +0200

    add article number

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ END = None
 
 st.set_page_config(
     #initial_sidebar_state="collapsed",
-    layout ="centered",
-    page_title="HarvAI - V1 - Test",
-    page_icon=":robot:"
+    layout ="wide",
+    page_title="HarvAI",
+    page_icon=":desktop_computer:"
 )
 
 #load local env variable if not on heroku else load heroku env variable
@@ -31,14 +31,20 @@ else:
 
 #headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-st.markdown("<h1 style='text-align: center; color: white;'>HarvAI</h1>", unsafe_allow_html=True)
 
+# ------------ Side Bar------------
+
+from PIL import Image
+image = Image.open('images/robot_reading.png')
+st.sidebar.image(image)
+st.sidebar.markdown("<div><h1 style='text-align: center; color: white;'>HarvAI</h1></div>", unsafe_allow_html=True)
+st.sidebar.markdown("This chat bot allows find answer on your french traffic regulation question using Python and Streamlit.")
+st.sidebar.markdown("To get started <ol><li>Write the question you wish to ask in the question bar and press enter</li> <li>You can finetune parameters below.</li></ol>",unsafe_allow_html=True)
 
 # ------------ Parameters------------
 
-
 st.sidebar.markdown(f"""
-    ## PARAMETERS
+    ## Parameters :
     """)
 
 retriever =st.sidebar.radio('Select a Retriever :', ('KNN', 'BM25', 'DPR', 'Embedding'),index=3)
@@ -57,50 +63,57 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 def get_text():
-    input_text = st.text_input("You: ","", key="input")
+    input_text = st.text_input("Question : ","", key="input")
     return input_text
 
-user_input = get_text()
+col1, col_separateur, col2= st.columns((5,1,5))
 
-if user_input:
+with col1 :
+    user_input = get_text()
 
-    output = requests.get("http://127.0.0.1:8000/answer", params={'question': user_input, 'retriever' : retriever, 'article_number' : nb_articles})
-    print(output)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output.json()['answer']['answer'])
+    if user_input:
 
-    ARTICLES = output.json()["parsed_context"]
-    START = output.json()['answer']['start']
-    END = output.json()['answer']['end']
-
-    print(retriever, nb_articles, reader_generator)
-    print(output.json()['answer']['answer'], output.json()['parsed_context'])
+        output = requests.get("http://127.0.0.1:8000/answer", params={'question': user_input, 'retriever' : retriever, 'article_number' : nb_articles})
+        print(output)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output.json()['answer']['answer'])
 
 
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
+        ARTICLES = output.json()["parsed_context"]
+        ARTICLES_REFERENCE = output.json()["article_reference"]
+        START = output.json()['answer']['start']
+        END = output.json()['answer']['end']
+
+        print(ARTICLES_REFERENCE)
+        print(output.json()['answer']['answer'], output.json()['parsed_context'])
+
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
 
 
 # ------------ Articles Expander ------------
 
 
-def hightlight(article, start, end):
+def hightlight(articles, start, end, reference):
     new = ""
-    for count, word in enumerate(list(article)):
-        if count == int(start) and start > 0:
-            new = new + " <mark style='background-color: DodgerBlue;'>" + word
-        elif count == int(end) and end > 0:
-            new = new + word + "</mark>"
-        else:
-            new = new + word
+    for number, article in enumerate(articles):
+        new = new + "<b>" + str(reference[number]) + ":" + "</b> <br>"
+        for count, word in enumerate(list(article)):
+            if count == int(start) and start > 0:
+                new = new + " <mark style='background-color: DodgerBlue;'>" + word
+            elif count == int(end) and end > 0:
+                new = new + word + "</mark>"
+            else:
+                new = new + word
+        new = new + "<p></p>"
+        start = start - len(article)
+        end = end - len(article)
     return new
 
-with st.expander("See Articles Returned"):
-
+with col2:
+    st.markdown("Articles Returned :")
     if ARTICLES is not None:
-        for article2 in ARTICLES:
-            st.markdown(hightlight(article2, START, END), unsafe_allow_html=True)
-            START = START - len(article2)
-            END = END - len(article2)
+        st.markdown(hightlight(ARTICLES, START, END, ARTICLES_REFERENCE), unsafe_allow_html=True)
